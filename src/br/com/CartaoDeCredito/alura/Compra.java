3d41f09f54ffe25a3c6dde9214f41f3195af9073
Lucas <lucasdecassia9@hotmail.com>
2023-05-03 15:42:03 -0300

    Classes modelos, implementandos metodos, e a criação da classe StartUp

---
+++ src/br/com/CartaoDeCredito/alura/Compra.java
@@ -0,0 +1,24 @@
+package br.com.CartaoDeCredito.alura;
+
+public class Compra {
+    private String descricao;
+    private double valor;
+
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compras: descricao" + descricao + " valor: " + valor;
+    }
+}
